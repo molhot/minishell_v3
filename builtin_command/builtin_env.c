64b4b4c3014f1b3420ae7e577c79d81189a87a8a
molhot <sakata19991214@gmail.com>
2023-03-12 20:31:27 +0900

    env recover

--- builtin_command/builtin_env.c
+++ builtin_command/builtin_env.c
@@ -3,23 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_env.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mochitteiunon? <sakata19991214@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 20:20:06 by user              #+#    #+#             */
-/*   Updated: 2023/03/02 10:08:25 by marvin           ###   ########.fr       */
+/*   Updated: 2023/03/12 20:30:03 by mochitteiun      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static char	*value_ready(char *value)
+{
+	char	*new_value;
+
+	new_value = NULL;
+	while (*value != '\0')
+	{
+		if (*value == '\\')
+			value++;
+		append_char(&new_value, *value);
+		value++;
+	}
+	return (new_value);
+}
+
 void	ms_env(void)
 {
 	t_item	*itr;
+	char	*value_cpy;
 
 	itr = g_env->item_head;
 	while (itr != NULL)
 	{
-		printf("%s=%s\n", itr->name, itr->value);
+		value_cpy = value_ready(itr->value);
+		printf("%s=%s\n", itr->name, value_cpy);
 		itr = itr->next;
+		free(value_cpy);
 	}
 }
